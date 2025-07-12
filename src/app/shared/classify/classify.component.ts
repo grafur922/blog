7f094cd096e9aef7ac384587148214155aa2f00c
sunli <SL3103433522@163.com>
2025-07-12 17:05:21 +0800

    锚点跳转

--- src/app/shared/classify/classify.component.ts
+++ src/app/shared/classify/classify.component.ts
@@ -3,15 +3,17 @@ import { cardTmpComponent } from "../cardTmp/cardTmp.component";
 import axios from 'axios'
 import { ClassifyDataService } from '../../shared/services/classifyData/classify-data.service';
 import { LoggerService } from '../services/Logger/logger.service';
+import { Router , RouterModule} from '@angular/router';
 @Component({
     selector:'classify',
     templateUrl:'./classify.component.html',
     styleUrl:'./classify.component.less',
-    imports: [cardTmpComponent]
+    imports: [cardTmpComponent,RouterModule]
 })
 export class ClassifyComponent implements OnInit{
     constructor(public data:ClassifyDataService){}
     iconName:string='fa-list'
+    router=inject(Router)
     Logger=inject(LoggerService)
     ngOnInit(): void {
         axios.get('/api/getClassify').then(res=>{
