774918e20d42c871413290c1491f7bf4cd2dcce7
sunli <SL3103433522@163.com>
2025-07-14 16:22:59 +0800

    直接expanded面板

--- src/app/pages/classify-viewer/classify-viewer.component.ts
+++ src/app/pages/classify-viewer/classify-viewer.component.ts
@@ -1,31 +1,46 @@
 import { Component, inject, OnInit } from '@angular/core';
-import {MatExpansionModule,MatAccordionTogglePosition} from '@angular/material/expansion'
+import { MatExpansionModule } from '@angular/material/expansion';
 import { HeaderComponent } from "../../core/header/header.component";
 import { ClassifyDataService } from '../../shared/services/classifyData/classify-data.service';
 import { AsyncPipe } from '@angular/common';
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs';
 import { DataService } from '../../shared/services/data.service';
 import { classify } from '../../shared/interfaces/classifyData';
 import { ArticleFormData } from '../../shared/interfaces/articleFormData';
 import { NavToViewService } from '../../shared/services/navToView/nav-to-view.service';
-import { Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-classify-viewer',
-  imports: [MatExpansionModule, HeaderComponent,AsyncPipe],
+  imports: [MatExpansionModule, HeaderComponent, AsyncPipe],
   templateUrl: './classify-viewer.component.html',
   styleUrl: './classify-viewer.component.less'
 })
-export class ClassifyViewerComponent implements OnInit{
+export class ClassifyViewerComponent implements OnInit {
   classifyData$: Observable<classify[] | null>;
   articleData$: Observable<ArticleFormData[] | null>;
-  navService=inject(NavToViewService)
-  constructor(private classifyDataService:ClassifyDataService,private articleDataService:DataService){
-    this.classifyData$=this.classifyDataService.classifyData$
-    this.articleData$=this.articleDataService.data$
+  navService = inject(NavToViewService);
+
+  expandedClassifyName: string | null = null;
+
+  constructor(
+    private classifyDataService: ClassifyDataService,
+    private articleDataService: DataService,
+    private route: ActivatedRoute
+  ) {
+    this.classifyData$ = this.classifyDataService.classifyData$;
+    this.articleData$ = this.articleDataService.data$;
   }
+
   ngOnInit(): void {
-    this.classifyDataService.loadData()
-    this.articleDataService.loadData()
+    this.expandedClassifyName = this.route.snapshot.fragment;
+    this.classifyDataService.loadData();
+    this.articleDataService.loadData();
   }
-  panelOpenState=false
+
+  handlePanelClosed(classifyName: string): void {
+    if (this.expandedClassifyName === classifyName) {
+      this.expandedClassifyName = null;
+    }
+  }
 }
