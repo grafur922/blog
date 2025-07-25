fbe42e88f3da6becc6bcb8b62fa485a033c1f8f6
sunli <SL3103433522@163.com>
2025-07-25 15:26:13 +0800

    新

--- src/app/shared/archive/archive.component.ts
+++ src/app/shared/archive/archive.component.ts
@@ -1,12 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { cardTmpComponent } from "../cardTmp/cardTmp.component";
+import { ArticleFormData } from "../interfaces/articleFormData";
+import { DataService } from "../services/data.service";
+import { Observable } from "rxjs";
 @Component({
     selector:'archive',
     templateUrl:'./archive.component.html',
     imports:[cardTmpComponent],
     styleUrl:'./archive.component.less'
 })
-export class ArchiveComponent{
-    constructor(){}
+export class ArchiveComponent implements OnInit{
+    articleData$:Observable<ArticleFormData[] |null>|null=null
+    constructor(private articleService:DataService){
+        this.articleData$=articleService.data$
+    }
     iconName:string='fa-inbox'
+    ngOnInit(): void {
+        // this.articleService.loadData()
+        this.articleData$?.subscribe(res=>{
+            console.log(res);
+            
+        })
+    }
 }
