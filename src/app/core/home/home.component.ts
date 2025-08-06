d528e6d7e4d4539ff58823bb61dd938ae6ec842c
sunli <SL3103433522@163.com>
2025-08-06 21:16:01 +0800

    Refactor homepage style

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -48,6 +48,7 @@ export class HomeComponent implements OnInit,AfterViewInit {
       },
       duration: 1
     });
+    // (new Promise).catch(res=>location.href(''))
   }
   ngAfterViewInit(): void {
       
