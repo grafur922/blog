6ab406ba117c6395b31b2bc3ec64d9a7b7bc6d89
sunli <SL3103433522@163.com>
2025-08-12 22:26:07 +0800

    fix scrollTrigger animation

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { LayoutComponent } from '../layout/layout.component';
 import { bgComponent } from '../background/bg.component';
@@ -15,29 +15,28 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit {
   loading=inject(LoadingService)
   isLoading=true
   home=viewChild<ElementRef<HTMLDivElement>>('home')
   constructor() {
     this.loading.loadCriticalAssets().subscribe(res=>{
       this.isLoading=false
+      setTimeout(() => {
+        this.startScrollAni()
+      }, 0);
+    })
+    this.loading.isLoading$.subscribe(res=>{
+      if(res===false){
+        
+      }
     })
     // this.loading.isLoading$.subscribe(res=>{
     //   console.log('loading state is ' + res);
       
     // })
   }
-  show(){
-    
-  }
-  ngOnInit(): void {
-    let smoother = ScrollSmoother.create({
-      smooth: 1,
-      smoothTouch: 0.1,
-      effects: true,
-      // normalizeScroll: true
-    });
+  startScrollAni(){
     gsap.to('.top', {
       scrollTrigger: {
         trigger: '.top',
@@ -49,6 +48,21 @@ export class HomeComponent implements OnInit {
       },
       duration: 1
     });
+  }
+  ngAfterViewInit(): void {
+      
+  }
+  show(){
+    
+  }
+  ngOnInit(): void {
+    let smoother = ScrollSmoother.create({
+      smooth: 1,
+      smoothTouch: 0.1,
+      effects: true,
+      // normalizeScroll: true
+    });
+    
     // (new Promise).catch(res=>location.href(''))
   }
 }
