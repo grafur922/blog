a7596a5d6f7eb41ee4e101f30e4dc5afc30e55cb
sunli <SL3103433522@163.com>
2025-09-03 09:40:17 +0800

    refactor(category): implement checkbox selection for categories and update related components

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChild,NgZone } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChild, NgZone } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { LayoutComponent } from '../layout/layout.component';
 import { bgComponent } from '../background/bg.component';
@@ -7,7 +7,6 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { ScrollSmoother } from 'gsap/all';
 import { LoadingScreenComponent } from "../loading-screen/loading-screen.component";
-// import {outof}
 import { LoadingService } from '../../shared/services/loading/loading.service';
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 @Component({
@@ -16,29 +15,30 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent implements OnInit,AfterViewInit {
-  loading=inject(LoadingService)
-  ngZone=inject(NgZone)
-  isLoading=true
-  home=viewChild<ElementRef<HTMLDivElement>>('home')
+export class HomeComponent implements OnInit, AfterViewInit {
+  loading = inject(LoadingService)
+  ngZone = inject(NgZone)
+  isLoading = true
+  home = viewChild<ElementRef<HTMLDivElement>>('home')
+  aniStart:ReturnType<typeof setTimeout>|null=null
   constructor() {
-    this.loading.loadCriticalAssets().subscribe(res=>{
-      this.isLoading=false
-      this.ngZone.runOutsideAngular(()=>{
-        setTimeout(() => {
-        this.startScrollAni()
-      }, 100);
+    this.loading.loadCriticalAssets().subscribe(res => {
+      this.isLoading = false
+      this.ngZone.runOutsideAngular(() => {
+        this.aniStart=setTimeout(() => {
+          this.startScrollAni()
+        }, 100);
       })
-      
+
     })
-    this.loading.isLoading$.subscribe(res=>{
+    this.loading.isLoading$.subscribe(res => {
     })
     // this.loading.isLoading$.subscribe(res=>{
     //   console.log('loading state is ' + res);
-      
+
     // })
   }
-  startScrollAni(){
+  startScrollAni() {
     gsap.to('.top', {
       scrollTrigger: {
         trigger: '.top',
@@ -50,26 +50,29 @@ export class HomeComponent implements OnInit,AfterViewInit {
       },
       duration: 1
     });
+    if(this.aniStart){
+      clearTimeout(this.aniStart)
+    }
   }
 
 
   ngAfterViewInit(): void {
-      
+
   }
-  show(){
-    
+  show() {
+
   }
   ngOnInit(): void {
-    this.ngZone.runOutsideAngular(()=>{
+    this.ngZone.runOutsideAngular(() => {
       let smoother = ScrollSmoother.create({
-      smooth: 1,
-      smoothTouch: 0.1,
-      effects: true,
-      // normalizeScroll: true
-    });
+        smooth: 1,
+        smoothTouch: 0.1,
+        effects: true,
+        // normalizeScroll: true
+      });
     })
-    
-    
+
+
     // (new Promise).catch(res=>location.href(''))
   }
 }
--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,18 +1,16 @@
 import { Component } from "@angular/core";
-import { MyCardComponent } from "../../shared/myCard/myCard.component";
 import { CategoryComponent } from "../../shared/category/category.component";
 import { ArchiveComponent } from "../../shared/archive/archive.component";
 import { StatisticsComponent } from "../../shared/statistics/statistics.component";
 import { recentComponent } from "../../shared/recent/recent.component";
-import { RouterLink,RouterModule } from "@angular/router";
-import { FooterComponent } from "../footer/footer.component";
+import { RouterModule } from "@angular/router";
 import { ArticleListComponent } from "../article-list/article-list.component";
 // import { cardTmpComponent } from "../../shared/cardTmp/cardTmp.component";
 @Component({
     selector: 'layout',
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.less',
-    imports: [MyCardComponent, RouterModule, CategoryComponent, ArchiveComponent, StatisticsComponent, recentComponent, ArticleListComponent]
+    imports: [ RouterModule, CategoryComponent, ArchiveComponent, StatisticsComponent, recentComponent, ArticleListComponent]
 })
 export class LayoutComponent {
     constructor() {
